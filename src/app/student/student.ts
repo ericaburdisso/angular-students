0fcd82bfd3dd8da26495024b38d55347d467dd87
ericaburdisso <e.burdisso.3497@vallauri.edu>
2026-09-16 12:44:51 +0200

    creazione di una semplice lista di studenti con @for

---
+++ src/app/student/student.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  imports: [],
+  templateUrl: './student.html',
+  styleUrl: './student.css',
+})
+export class Student {
+  students: any[] =
+  [{name: "pippo", city: "topolino", gender: "M", present: true},
+  {name: "pluto", city: "topolino", gender: "M", present: false}, 
+  {name: "paperino", city: "paperopoli", gender: "M", present: true},
+  {name: "minnie", city: "topolina", gender: "F", present: true},
+  {name: "paperina", city: "topolino", gender: "F", present: false},
+  {name: "cerone", city: "topolino", gender: "M", present: true}
+  ];
+
+}
